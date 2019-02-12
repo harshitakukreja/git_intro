6c10d130d541ad178438a412a7153942c6ce9d52
Harshita Kukreja <harshitakukreja8@gmail.com>
2019-02-12 23:18:39 +0530

    modified greeter

--- file01.js
+++ file01.js
@@ -2,6 +2,7 @@ function greet(name) {
     console.log('knock knock knock ' + name)
     console.log('knock knock knock ' + name)
     console.log('knock knock knock ' + name)
+    console.log('Would you like something to eat?')
 }
 
 greet('harshita')
